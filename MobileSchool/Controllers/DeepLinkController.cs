654c8238001076f7bf3c3c63779d690f1d8658a9
tomer <tomer@sears.co.il>
2017-11-17 22:28:27 +0200

    New controller

---
+++ MobileSchool/Controllers/DeepLinkController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MobileSchool.Controllers
+{
+    public class DeepLinkController : Controller
+    {
+		public ActionResult DeepLink()
+		{
+			return Redirect("tomerdl://");
+		}
+
+
+		public ActionResult Intent()
+		{
+			return Redirect("intent://open?link_click_id=123456#Intent;scheme=tomerdl;package=com.link.deeplink.deeplinking;end");
+		}
+	}
+}
