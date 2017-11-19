7a220ca622b6ebb7e16414274d2855b9aab2acb0
tomer <tomer@sears.co.il>
2017-11-19 16:37:56 +0200

    javascript deeplinkinh

--- MobileSchool/Controllers/DeepLinkController.cs
+++ MobileSchool/Controllers/DeepLinkController.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-using System.Web.Mvc;
+﻿using System.Web.Mvc;
 
 namespace MobileSchool.Controllers
 {
-    public class DeepLinkController : Controller
+	public class DeepLinkController : Controller
     {
 		public ActionResult DeepLink()
 		{
@@ -17,6 +13,11 @@ namespace MobileSchool.Controllers
 		public ActionResult Intent()
 		{
 			return Redirect("intent://open?link_click_id=123456#Intent;scheme=tomerdl;package=com.link.deeplink.deeplinking;end");
+		}
+
+		public ActionResult DLView()
+		{
+			return View();
 		}
 	}
 }
